43a6670ca4f57bd9edf3cd16d6f76f1c146ecc00
OneBoat <boatt@users.noreply.github.com>
2021-04-30 02:54:25 +0800

    Update ASBase.java
    去除注釋

--- ashelp/src/main/java/com/xiaoniu/asmhelp/bean/ASBase.java
+++ ashelp/src/main/java/com/xiaoniu/asmhelp/bean/ASBase.java
@@ -3,16 +3,7 @@ package com.xiaoniu.asmhelp.bean;
 import java.io.Serializable;
 import java.util.List;
 
-/**
- * Desc:
- * <p>
- *
- * @author: ZhouTao
- * Date: 2019/7/8
- * Copyright: Copyright (c) 2016-2020
- * Company: @小牛科技
- * Update
- */
+
 public class ASBase implements Serializable {
 
     /**
